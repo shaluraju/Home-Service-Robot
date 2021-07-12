7c8ec02a8d76224246e18bac6fe0f7859d189bdd
Shalemuraju <shalemurajukatari@gmail.com>
2021-07-12 12:16:40 +0530

    first push

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# launch turtlebot_world.launch to deploy turtlebot environment
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x 0 -y 0 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../../src/map/u.world " & 
+
+sleep 5
+
+# launch amcl_demo.launch for localization
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../../src/map/mapnew.yaml " &
+
+sleep 5
+
+# launch view_navigation for rviz
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 20 # keeping large to enable visualization
+
+# launch pick_objects node
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+rosparam load $(pwd)/../config/marker_config.yaml;
+rosrun pick_objects pick_objects " &
---
+++ scripts/test_slam.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# launch turtlebot_world.launch to deploy turtlebot environment
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x 0 -y 0 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../../src/map/u.world " & 
+
+sleep 15
+
+# launch gmapping_demo.launch to perform slam_gmapping
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo gmapping_demo.launch" &
+
+sleep 5
+
+# launch view_navigation for rviz
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 10
+
+# launch teleop_keyboard for manual control
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_teleop keyboard_teleop.launch" &
+
+# save using following on command line
+# rosrun map_server map_saver -f <map-location-and-name>
